6fbe51108e073ddd6f7e24b6d133770d9b85a09f
Tyrone Smith <tyrone@vauntico.com>
2025-10-27 17:11:44 +0200

    fix: trigger fresh CSS build

---
+++ vauntico-mvp/vault-landing/.next/static/chunks/pages/index-b90b7805b2b3ef2f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[332],{2936:(_,e,n)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return n(3468)}])},3468:(_,e,n)=>{"use strict";n.r(e),n.d(e,{default:()=>u});let u=()=>"Vault Landing Alive"}},_=>{_.O(0,[636,593,792],()=>_(_.s=2936)),_N_E=_.O()}]);
