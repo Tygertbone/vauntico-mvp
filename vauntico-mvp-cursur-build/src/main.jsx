03b4e7781aa9c61198e4c5569dd33a6699d6f61e
Tyrone James <tyatjamesd@gmail.com>
2025-10-27 16:41:48 +0200

    fix: add .js extension to performance import

--- vauntico-mvp-cursur-build/src/main.jsx
+++ vauntico-mvp-cursur-build/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { initPerformanceMonitoring } from './utils/performance'
+import { initPerformanceMonitoring } from './utils/performance.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
